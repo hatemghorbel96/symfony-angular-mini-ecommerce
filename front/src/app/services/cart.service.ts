dfe80cf21aecca3e01f9719eab58d4187aa524e9
Ghorbel hatem <61474229+hatemghorbel96@users.noreply.github.com>
2022-10-26 20:54:40 +0100

    Update cart.service.ts

--- front/src/app/services/cart.service.ts
+++ front/src/app/services/cart.service.ts
@@ -16,12 +16,12 @@ export class CartService {
 
   addToCart(cart: CartItem) {
 
-    // check if we already have the item in our cart
+ 
     let alreadyExistsInCart: boolean = false;
     let existingCartItem!: CartItem;
 
     if (this.cartItems.length > 0) {
-      // find the item in the cart based on item id
+      
 
       for (let c of this.cartItems) {
         if (c.id === cart.id) {
@@ -30,7 +30,7 @@ export class CartService {
         }
       }
 
-      // check if we found it
+   
       alreadyExistsInCart = (existingCartItem != undefined);
     }
 
@@ -39,11 +39,11 @@ export class CartService {
       existingCartItem.quantity++;
     }
     else {
-      // just add the item to the array
+     
       this.cartItems.push(cart);
     }
 
-    // compute cart total price and total quantity
+   
     this.computeCartTotals();
   }
 
@@ -57,11 +57,11 @@ export class CartService {
       totalQuantityValue += currentCartItem.quantity;
     }
 
-    // publish the new values ... all subscribers will receive the new data
+    
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
 
-    // log cart data just for debugging purposes
+   
     this.logCartData(totalPriceValue, totalQuantityValue);
   }
 
@@ -91,10 +91,10 @@ export class CartService {
 
   remove(theCartItem: CartItem) {
 
-    // get index of item in the array
+    
     const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
 
-    // if found, remove the item from the array at the given index
+ 
     if (itemIndex > -1) {
       this.cartItems.splice(itemIndex, 1);
 
